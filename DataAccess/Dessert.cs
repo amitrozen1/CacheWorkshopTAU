c353f8bdc5b7b182170a53e4a1f155ab61ef7acd
Avital Ofstein <avitalof@payoneer.com>
2024-02-18 02:51:27 +0200

    Add project files.

---
+++ DataAccess/Dessert.cs
@@ -0,0 +1,8 @@
+﻿namespace DataAccess
+{
+    public class Dessert
+    {
+        public string DessertName { get; set; }
+        public int NumberOfIngredients { get; set; }
+    }
+}
---
+++ DataAccess/DessertsDataAccess.cs
@@ -0,0 +1,27 @@
+﻿
+namespace DataAccess
+{
+    public class DessertsDataAccess
+    {
+        public async Task<List<Dessert>> GetDesserts() 
+        {
+            List<Dessert> desserts = [];
+
+            desserts.Add(new Dessert { DessertName = "Chocolate Chip Cookies", NumberOfIngredients = 9 });
+            desserts.Add(new Dessert { DessertName = "Cheesecake", NumberOfIngredients = 11 });
+            desserts.Add(new Dessert { DessertName = "Brownies", NumberOfIngredients = 8 });
+            desserts.Add(new Dessert { DessertName = "Apple Pie", NumberOfIngredients = 11 });
+            desserts.Add(new Dessert { DessertName = "Tiramisu", NumberOfIngredients = 7 });
+
+            desserts.Add(new Dessert { DessertName = "Pumpkin Pie", NumberOfIngredients = 9 });
+            desserts.Add(new Dessert { DessertName = "Chocolate Mousse", NumberOfIngredients = 6 });
+            desserts.Add(new Dessert { DessertName = "Strawberry Shortcake", NumberOfIngredients = 8 });
+            desserts.Add(new Dessert { DessertName = "Creme Brulee", NumberOfIngredients = 4 });
+            desserts.Add(new Dessert { DessertName = "Molten Lava Cake", NumberOfIngredients = 7 });
+
+            await Task.Delay(3000);
+
+            return desserts;
+        }
+    }
+}
