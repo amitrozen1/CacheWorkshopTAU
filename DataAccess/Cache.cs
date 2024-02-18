a167ac5305acd112cf26ca6c701736b6107dca85
Avital Ofstein <avitalof@payoneer.com>
2024-02-18 13:09:38 +0200

    Generic cache structure

---
+++ DataAccess/Cache.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class Cache<TKey, TValue>
+    {
+        private Dictionary<TKey, TValue> _cache;
+
+        public Cache() 
+        {
+            _cache = new Dictionary<TKey, TValue>();
+        }
+        public TValue Get(TKey key)
+        {
+            // Implement get item by key from the cache
+            return default;
+        }
+
+        public void Set(TKey key, TValue value)
+        {
+            // Implement setting item by key to the cache
+        }
+    }
+}
